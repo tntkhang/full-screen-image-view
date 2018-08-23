a410e81ed72bb496e77595c0bb71679a471b43e1
KhangTran <khang.tran@axonactive.com>
2018-08-23 17:38:10 +0700

    Update version 1.1.1

--- full-screen-image-view-library/src/main/java/com/github/tntkhang/fullscreenimageview/library/FullScreenImageViewActivity.java
+++ full-screen-image-view-library/src/main/java/com/github/tntkhang/fullscreenimageview/library/FullScreenImageViewActivity.java
@@ -7,12 +7,12 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
 import java.util.ArrayList;
 
 public class FullScreenImageViewActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
     public static final String URI_LIST_DATA = "URI_LIST_DATA";
     public static final String IMAGE_FULL_SCREEN_CURRENT_POS = "IMAGE_FULL_SCREEN_CURRENT_POS";
 
@@ -21,10 +21,13 @@ public class FullScreenImageViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_touch_image_view);
 
-        toolbar = findViewById(R.id.toolbar);
+        findViewById(R.id.ic_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
         ViewPager viewPager = findViewById(R.id.view_pager);
-
-        setupToolbar();
 
         ArrayList<String> imagePaths = getIntent().getStringArrayListExtra(URI_LIST_DATA);
 
@@ -34,23 +37,5 @@ public class FullScreenImageViewActivity extends AppCompatActivity {
         PagerAdapter adapter = new PagerAdapter(manager, imagePaths);
         viewPager.setAdapter(adapter);
         viewPager.setCurrentItem(currentPos);
-    }
-
-    private void setupToolbar() {
-        setSupportActionBar(toolbar);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle("");
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
-        }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
